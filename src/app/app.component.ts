90912fbcaa78ffc0ce09640b25219c2f829ce715
Timur <scorpik1992@gmail.com>
2024-07-04 21:27:36 +0600

    made main page header ,horizontal scroll bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { itemTypeList } from './common/ItemTypes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shop';
+  itemTypeList=itemTypeList;
+  isBlinking = false;
+  isBlinkingIndex: number | null = null;
+  handlItemClick(item:string,index:number){
+    this.isBlinking = true;
+    this.isBlinkingIndex = index;
+    // Выключаем мигание через 1 секунду
+    setTimeout(() => {
+      this.isBlinkingIndex = null;
+      this.isBlinking = false;
+    }, 750);
+    console.log(item)
+  }
 }
