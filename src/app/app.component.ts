9e920bd70a85a3bd1f96cb07ee0d8f45451f98e4
Timur <scorpik1992@gmail.com>
2024-07-08 14:03:59 +0600

    fix bugs whith pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,20 +46,22 @@ export class AppComponent implements OnInit{
         debounceTime(300),
         distinctUntilChanged(),  
         tap(value => {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
           this.search = value !== ''; // Set the flag based on input value
         }),
         switchMap(query => this.InitsearchProducts(query))
         
       ).subscribe((data:any )=> {      
-        console.log(data)
+       // console.log(data)
         this.productList = data.results||[];
-        console.log(this.productList)
+      //  console.log(this.productList)
         this.search=true;        
         this.lastScrollPosition=0
-        this.threshold=-400
+        this.threshold=-300
         this.endOfList=false
-        this.isLoading=false
-        this.page=1;
+        this.isLoading=false      
+        if(!this.flag)  
+            this.page=1;
       });
       this.service.getCategory().subscribe((data:any)=>{
         this.itemTypeList.push({id:1,name:"Главная"},...data.results||[])
@@ -69,15 +71,17 @@ export class AppComponent implements OnInit{
 
   loadProducts() {
     this.isLoading = true;
-    console.log("laod")
-    if(this.search==false){
+   // console.log("laod")
+    if(this.search==false ||this.carrentquery==''){
+           console.log("get")          
            this.normalGet()
   }else {
+    console.log("get1")
       this.searchGet()
   }
    this.loading=true;
   }
-  threshold=-400;
+  threshold=-300;
   @HostListener('window:scroll', ['$event'])
   onScroll(event: any) {
      // Threshold in pixels before the end of the page
@@ -85,10 +89,15 @@ export class AppComponent implements OnInit{
     const height = document.body.offsetHeight;
     //console.log(height - position)
     // Check if the user is scrolling down and approaching the bottom of the page
-    console.log(this.isLoading )
+    if (window.scrollY < this.lastScrollPosition) {
+      console.log("loadin to true ")
+      this.isLoading = true; // Reset isLoading flag when scrolling up
+    }
+   console.log(this.isLoading )
     console.log(this.endOfList )
+
     if (height - position <= this.threshold && !this.isLoading && !this.endOfList) {
-      this.threshold=this.threshold-180;
+      this.threshold=this.threshold-150;
 
       this.loadProducts();
       this.isLoading = true; // Set isLoading flag to true to prevent concurrent requests
@@ -96,14 +105,13 @@ export class AppComponent implements OnInit{
     }
     
     // Additional check to prevent loading if the user scrolls back up to an area that was already loaded
-    if (window.scrollY < this.lastScrollPosition) {
-      this.isLoading = true; // Reset isLoading flag when scrolling up
-    }
+
   
     this.lastScrollPosition = window.scrollY; // Update last scroll position
   }
   handlItemClick(item:string,index:number){
-    this.threshold=-400
+    this.lastScrollPosition=0;
+    this.threshold=-300
     this.productList=new Array();
     this.page=1;
     if(this.currentProductType==item)
@@ -111,14 +119,16 @@ export class AppComponent implements OnInit{
     this.isBlinking = true;
     this.isBlinkingIndex = index;
     this.endOfList=false
+    this.isLoading=true;
     // Выключаем мигание через 1 секунду
+    window.scrollTo(0, 0)
     setTimeout(() => {
       this.isBlinkingIndex = null;
       this.isBlinking = false;
     }, 750);
     //console.log(item)
     this.currentProductType=item;
-    if(this.search==false){
+    if(this.search==false || this.carrentquery==''){
     this.normalGet();
       }else{
         this.searchProducts(this.carrentquery);
@@ -130,7 +140,7 @@ export class AppComponent implements OnInit{
     return this.service.getByCategory(this.currentProductType,this.page, this.pageSize).subscribe(
       (data: any) => {
         let list=data.results||[]
-       console.log(list)
+      // console.log(list)
         if (list.length > 0) {
           this.productList.push(...data.results);
           this.page++;
@@ -138,6 +148,7 @@ export class AppComponent implements OnInit{
       //    console.log(this.productList)
         } else {
           this.endOfList = true; 
+          this.page=1
         // console.log("end")// Помечаем, что больше товаров нет
         }
         this.isLoading = false;
@@ -150,7 +161,7 @@ export class AppComponent implements OnInit{
    
   }
   searchGet(){
-  console.log("search")
+  //console.log("search")
     return this.service.searchProducts(this.carrentquery,this.currentProductType,this.page, this.pageSize).subscribe(
       (data: any) => {
         let list=data.results||[]
@@ -162,6 +173,7 @@ export class AppComponent implements OnInit{
       //    console.log(this.productList)
         } else {
           this.endOfList = true; 
+          this.page=1;
         // console.log("end")// Помечаем, что больше товаров нет
         }
         this.isLoading = false;
@@ -180,19 +192,22 @@ export class AppComponent implements OnInit{
       return this.searchGet();
 
   }  
+  flag:boolean=false;
   private InitsearchProducts(query: string) {
 
     this.carrentquery=query
     if(this.carrentquery==''){
       this.search=false
-      console.log("defulat loading") 
-      this.page=this.page+1;
+      //console.log("defulat loading") 
+      this.page=2;
       this.isLoading=false
-      this.threshold=-400
+      this.threshold=-300
+      this.flag=true
       return this.service.getByCategory(this.currentProductType,1,this.pageSize);
     }
-    this.threshold=-400
+    this.threshold=-300
     this.isLoading=false
+    this.page=2;
     return this.service.searchProducts(query,this.currentProductType,1,this.pageSize);
 
   }
